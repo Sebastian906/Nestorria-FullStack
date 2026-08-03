1fe56886b313fa9387ee47c6d908b0b90be4d05b
Sebastian906 <elsebas1912@gmail.com>
2026-08-03 12:41:59 -0500

    stripe fix to redirect to checkout

--- server/src/main/java/com/nestorria/server/common/event/InvoicePaidEventListener.java
+++ server/src/main/java/com/nestorria/server/common/event/InvoicePaidEventListener.java
@@ -3,6 +3,7 @@ package com.nestorria.server.common.event;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -41,7 +42,7 @@ public class InvoicePaidEventListener {
     }
 
     @Async("taskExecutor")
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleInvoicePaid(InvoicePaidEvent event) {
         try {
